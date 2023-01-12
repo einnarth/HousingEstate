2ff2f9e226bc2f8126ac8fa7a7be877b91e4efcb
einnarth <113421210+einnarth@users.noreply.github.com>
2023-01-12 08:29:09 +0100

    Update 1. Created class Person.cs and created properties and fields Implemented Constructor for Person.cs Made a method to show information about certain Person in Program.cs

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,42 @@
+namespace HousingEstate
+{
+    public class Person
+    {
+        private string _name;
+        private string _surname;
+        private int _age;
+
+        public string Naming
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Surnaming
+        {
+            get => _surname;
+            set => _surname = value;
+        }
+
+        public int Ageing
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public Person(string name, string surname, int age)
+        {
+            this._name = name;
+            this._surname = surname;
+            this._age = age;
+        }
+        
+
+            public override string ToString()
+             {
+                  return string.Format(
+                $"Name:{Naming} \nSurname:{Surnaming} \nAge:{Ageing}");
+             }
+        
+    }
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HousingEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inhabitant = new Person("Michal", "Bernát", 16);
+            Console.Write(inhabitant.ToString());
+        }
+    }
+}
